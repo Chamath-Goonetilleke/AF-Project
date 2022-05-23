d860d2e0494248bd4c603c3d19dbed784a0bb3b9
Vihanga Pramudith <88036867+VihangaPramudith@users.noreply.github.com>
2022-05-23 20:29:31 +0530

    Create research group model

---
+++ models/groups.js
@@ -0,0 +1,31 @@
+const { boolean } = require("joi");
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const groupSchema = new Schema({
+  groupid: {
+    type: String,
+    required: true,
+  },
+  supercisorid: {
+    type: String,
+  },
+  cosupercisorid: {
+    type: String,
+  },
+  panelmember: {
+    type: String,
+  },
+
+  report: String,
+  presentation: String,
+  proposal: String,
+  Finalthesis: String,
+  isOngoing: {
+    type: Boolean,
+  },
+});
+
+const Group = mongoose.model("researchgroups", groupSchema);
+module.exports = Group;
